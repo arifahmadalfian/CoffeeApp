e78f2b72d4e66e8ea183bb75b347a80e94d764c7
Arief Ahmad Alfian <49640973+arifahmadalfian@users.noreply.github.com>
2022-12-01 21:55:49 +0700

    Initial commit

---
+++ app/src/main/java/com/arifahmadalfian/coffeeapp/MainActivity.kt
@@ -0,0 +1,162 @@
+package com.arifahmadalfian.coffeeapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.arifahmadalfian.coffeeapp.model.*
+import com.arifahmadalfian.coffeeapp.ui.components.*
+import com.arifahmadalfian.coffeeapp.ui.theme.CoffeeAppTheme
+import com.arifahmadalfian.coffeeapp.ui.theme.LightGray
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CoffeeAppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    JetCoffeeApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun JetCoffeeApp() {
+    Scaffold(
+        bottomBar = { BottomBar() }
+    ) {
+        Column(
+            modifier = Modifier
+                .verticalScroll(rememberScrollState())
+                .padding(it)
+        ) {
+            Banner()
+            HomeSection(title = stringResource(id = R.string.section_category)) {
+                CategoryRow()
+            }
+            HomeSection(title = stringResource(id = R.string.section_favorite_menu)) {
+                MenuRow(listMenu = dummyMenu)
+            }
+            HomeSection(title = stringResource(id = R.string.section_best_seller_menu)) {
+                MenuRow(listMenu = dummyBestSellerMenu)
+            }
+        }
+    }
+}
+
+@Composable
+fun Banner(
+    modifier: Modifier = Modifier
+) {
+    Box(modifier = modifier) {
+        Image(
+            painter = painterResource(id = R.drawable.banner),
+            contentDescription = "Banner",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.height(160.dp)
+        )
+        SearchBar()
+    }
+}
+
+@Composable
+fun CategoryRow(
+    modifier: Modifier = Modifier
+) {
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        modifier = modifier
+    ) {
+        items(dummyCategory, key = { it.textCategory }) { category ->
+            CategoryItem(category = category)
+        }
+    }
+}
+
+@Composable
+fun MenuRow(
+    listMenu: List<Menu>,
+    modifier: Modifier = Modifier
+) {
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        modifier = modifier
+    ) {
+        items(listMenu, key = { it.title }) { menu ->
+            MenuItem(menu = menu)
+        }
+    }
+}
+
+@Composable
+fun BottomBar(
+    modifier: Modifier = Modifier
+) {
+    BottomNavigation(
+        backgroundColor = MaterialTheme.colors.background,
+        contentColor = MaterialTheme.colors.primary,
+        modifier = modifier
+    ) {
+        val navigationItems = listOf(
+            BottomBarItem(
+                title = stringResource(id = R.string.menu_home),
+                icon = Icons.Default.Home
+            ),
+            BottomBarItem(
+                title = stringResource(id = R.string.menu_favorite),
+                icon = Icons.Default.Favorite
+            ),
+            BottomBarItem(
+                title = stringResource(id = R.string.menu_profile),
+                icon = Icons.Default.AccountCircle
+            )
+        )
+        navigationItems.map {
+            BottomNavigationItem(
+                icon = {
+                    Icon(imageVector = it.icon, contentDescription = it.title)
+                },
+                label = {
+                    Text(it.title)
+                },
+                selected = it.title == navigationItems[0].title,
+                unselectedContentColor = LightGray,
+                onClick = { }
+            )
+        }
+    }
+}
+
+
+@Preview(showBackground = true, device = Devices.PIXEL_4)
+@Composable
+fun JetCoffeeAppPreview() {
+    CoffeeAppTheme {
+        JetCoffeeApp()
+    }
+}
---
+++ app/src/main/java/com/arifahmadalfian/coffeeapp/ui/components/CategoryItem.kt
@@ -0,0 +1,41 @@
+package com.arifahmadalfian.coffeeapp.ui.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.paddingFromBaseline
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.arifahmadalfian.coffeeapp.model.Category
+
+@Composable
+fun CategoryItem(
+    category: Category,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = category.imageCategory),
+            contentDescription = null,
+            modifier = Modifier
+                .size(60.dp)
+                .clip(CircleShape)
+        )
+        Text(
+            text = stringResource(id = category.textCategory),
+            fontSize = 10.sp,
+            modifier = Modifier.paddingFromBaseline(top = 16.dp, bottom = 8.dp)
+        )
+    }
+}
